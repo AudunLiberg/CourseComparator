6fa74bda7330d12668aaceb23fd35160aeecea34
Audun Liberg <audun.liberg@gmail.com>
2016-09-18 21:23:33 +0200

    Use nltk to tokenize course descriptions

--- courses.py
+++ courses.py
@@ -1,10 +1,14 @@
 import requests, sys, os, pickle
 from stringUtil import cleanText
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.data import load as nltk_load
 
 class Course:
    def __init__(self, data):
       self.data = data
       self.setDescription()
+      self.tokenize()
 
    def setDescription(self):
       self.description = self.data['name'] + ". "
@@ -14,6 +18,17 @@ class Course:
       for info in self.data['infoType']:
          if info['code'] in interestingInfoCodes and 'text' in info:
             self.description += cleanText(info['text']) + " "
+
+   def tokenize(self):
+      sentenceDetector = nltk_load('tokenizers/punkt/english.pickle')
+      sentences = sentenceDetector.tokenize(self.description.strip())
+      self.sentences = []
+      self.sentencesWithoutStopwords = []
+      for sentence in sentences:
+         tokens = word_tokenize(sentence)
+         tokensWithoutStopwords = [word for word in tokens if word.lower() not in stopwords.words('english')]
+         self.sentences.append(tokens)
+         self.sentencesWithoutStopwords.append(tokensWithoutStopwords)
 
 def isPickled():
    return os.path.isdir("data")
